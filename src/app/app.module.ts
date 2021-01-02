c8283ab30cecdf109a3deabfe0bec1d41081b619
Przemyslaw Zochowski <przemyslaw.zochowski@exorigo-upos.pl>
2021-01-02 17:02:54 +0100

    Starting new angular material project representing photographic portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
+import {MaterialModule} from './shared/material.module';
+import { MainDisplayComponent } from './components/main-display/main-display.component';
+import {FormsModule} from '@angular/forms';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+const routes: Routes = [
+  // { path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)},
+  { path: '**', redirectTo: 'main'},
+  { path: 'main', component: MainDisplayComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainDisplayComponent,
+    MainNavComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
