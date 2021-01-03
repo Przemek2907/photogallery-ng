34868b2f42e64bc9d6a73fa4e74226887b2e7d32
Przemyslaw Zochowski <przemyslaw.zochowski@exorigo-upos.pl>
2021-01-03 19:41:48 +0100

    Working on the hero component. Need to make menu not transparent when scroll is activated

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -10,11 +10,20 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class MainNavComponent {
 
+  navbarTransparent = false;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+
+  @HostListener('document:scroll')
+  onScroll() {
+    console.log('Was i here???');
+    this.navbarTransparent = document.body.scrollTop > 0;
+  }
+
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {Album} from '../../model/Album';
 
 @Component({
