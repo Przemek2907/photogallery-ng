8b8c5230021f71688e51e9aef78b29ccd23432e9
Przemyslaw Zochowski <przemyslaw.zochowski@exorigo-upos.pl>
2021-01-04 09:40:19 +0100

    Fixing navbar transparency going solid when scrolling

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -10,21 +10,18 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class MainNavComponent {
 
-  navbarTransparent = false;
-
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  @HostListener('document:scroll')
-  onScroll() {
-    console.log('Was i here???');
-    this.navbarTransparent = document.body.scrollTop > 0;
-  }
-
-
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  @HostListener('scroll', ['$event'])
+  onScroll(event: any) {
+    const navBarElement = document.getElementById('mainToolbar');
+    navBarElement.style.backgroundColor = event.target.scrollTop > 0 ? '#e9e4d6' : 'transparent';
+    }
 
 }
